2dc5da45d088e7f67db190257fb99d24bd761705
Andrew Eames <ajerxs@gmail.com>
2022-04-19 21:41:25 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,12 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, restaurantId, deleteReview } = this.props
+    const restaurantReviews = reviews.filter(review => review.restaurantId === restaurantId)
+    const renderedReviews = restaurantReviews.map(review => <Review key={review.id} review={review} deleteReview={deleteReview} />)
     return (
       <ul>
-        Reviews
+        {renderedReviews}
       </ul>
     );
   }
