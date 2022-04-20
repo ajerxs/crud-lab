f455fe67c8e7b352b567c7d3cdee867b186f9818
Andrew Eames <ajerxs@gmail.com>
2022-04-19 21:41:01 -0400

    commit tree 7bb5aaa7f8672db97a10fd00dcf24d383c8e84f2

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({
+      text: ''
+    })
+    event.target.reset();
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" onChange={this.handleOnChange} placeholder="Restaurant"></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
